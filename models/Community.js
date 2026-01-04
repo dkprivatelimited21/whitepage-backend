510e601a6bca8c77bbb4f2b2a65088d0bd67f375
unknown <everythingfree4200@gmail.com>
2026-01-04 20:12:25 +0530

    4

--- models/Community.js
+++ models/Community.js
@@ -46,18 +46,31 @@ const communitySchema = new mongoose.Schema({
     default: true
   }
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
-// Virtual for member count
-communitySchema.virtual('memberCount').get(function() {
+// REMOVE THIS VIRTUAL (it conflicts with the real memberCount field)
+// communitySchema.virtual('memberCount').get(function() {
+//   return this.members ? this.members.length : 0;
+// });
+
+// Instead, update the real memberCount field automatically
+communitySchema.pre('save', function(next) {
+  // Update memberCount based on members array length
+  this.memberCount = this.members ? this.members.length : 0;
+  next();
+});
+
+// Add a virtual for calculated member count if needed
+communitySchema.virtual('calculatedMemberCount').get(function() {
   return this.members ? this.members.length : 0;
 });
 
-// Middleware to update member count
-communitySchema.pre('save', function(next) {
-  this.memberCount = this.members ? this.members.length : 0;
-  next();
+// Add a virtual for the community URL
+communitySchema.virtual('url').get(function() {
+  return `/r/${this.name}`;
 });
 
 module.exports = mongoose.model('Community', communitySchema);
